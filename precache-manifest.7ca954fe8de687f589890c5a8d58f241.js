c209892ccbbe837131167df221e236e27f44af85
AlexCherny962 <53741736+AlexCherny962@users.noreply.github.com>
2020-08-05 06:47:21 +0300

    Updates

---
+++ precache-manifest.7ca954fe8de687f589890c5a8d58f241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7297fecf01fc01efd07689b7ce1a340c",
+    "url": "/cra18-github/index.html"
+  },
+  {
+    "revision": "f94d7e00b37f7d42dfff",
+    "url": "/cra18-github/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "da9ae0b9dbc03ea8f446",
+    "url": "/cra18-github/static/js/2.bbcc65c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cra18-github/static/js/2.bbcc65c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f94d7e00b37f7d42dfff",
+    "url": "/cra18-github/static/js/main.a960ec6a.chunk.js"
+  },
+  {
+    "revision": "b8bbc2bfb1dc4c080833",
+    "url": "/cra18-github/static/js/runtime-main.a0afb44d.js"
+  }
+]);
